355bc1bef13847cdd312bfa5a95fd20cc607dc6b
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-06-24 15:53:58 +0900

    fix config

--- AI/setup.sh
+++ AI/setup.sh
@@ -1,6 +1,6 @@
-pip install --upgrade pip
-pip install ultralytics
 python3 -m venv venv
 source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install ultralytics
 
 echo 'To exit the venv, run "deactivate"'
