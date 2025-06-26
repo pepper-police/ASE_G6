c33b9f9b4c85ec4065aa7d4ab825f39acf6e16f2
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-06-26 17:11:54 +0900

    fix setup script

--- AI/setup.sh
+++ AI/setup.sh
@@ -1,6 +1,42 @@
-python3 -m venv venv
-source venv/bin/activate
-pip3 install --upgrade pip
-pip3 install ultralytics
+#!/bin/bash
 
-echo 'To exit the venv, run "deactivate"'
+set -e
+
+echo "setup YOLO env"
+
+# Python3のインストール確認とインストール
+if ! type python3 &> /dev/null
+then
+    echo "'python3' not found, install 'python3'..."
+    sudo apt update
+    sudo apt install -y python3
+else
+    echo "'Python3' found"
+fi
+
+# pipのインストール確認とインストール
+if ! type pip3 &> /dev/null
+then
+    echo "'pip3' not found, install 'pip3'..."
+    sudo apt install -y python3-pip
+else
+    echo "'pip3' found"
+fi
+
+# venvの作成
+if [ ! -d "venv" ]; then
+    echo "creating virtual env..."
+    python3 -m venv venv
+else
+    echo "'venv' already exists"
+fi
+
+# venvの有効化
+echo "activate virtual env..."
+source venv/bin/activate
+
+# ultralyticsのインストール
+echo "Installing ultralytics..."
+pip install ultralytics
+
+echo 'Done'
