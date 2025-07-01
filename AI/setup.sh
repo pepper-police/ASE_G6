fc25aa6aa4491c3e3e42fe8a0541cf36caa47e68
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-07-01 14:37:17 +0900

    update setup script

--- AI/setup.sh
+++ AI/setup.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
 set -e
-
 echo "setup YOLO env"
-
-# Python3のインストール確認とインストール
-if ! type python3 &> /dev/null
-then
-    echo "'python3' not found, install 'python3'..."
-    sudo apt update
-    sudo apt install -y python3
-else
-    echo "'Python3' found"
-fi
-
-# pipのインストール確認とインストール
-if ! type pip3 &> /dev/null
-then
-    echo "'pip3' not found, install 'pip3'..."
-    sudo apt install -y python3-pip
-else
-    echo "'pip3' found"
-fi
+sudo apt update
+sudo apt install python3 python3-venv pip3 -y
 
 # venvの作成
 if [ ! -d "venv" ]; then
