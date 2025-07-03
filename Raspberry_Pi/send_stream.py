e9adfcc2b0103f2ba4e58c382e8271a2081c4b1c
ten <shimensoka000@gmail.com>
2025-07-03 19:13:38 +0900

    ストリーミング形式で送信するプログラムを書き換えた

--- Raspberry_Pi/send_stream.py
+++ Raspberry_Pi/send_stream.py
@@ -1,16 +1,13 @@
 # run this program on each RPi to send a labelled image stream
 # you can run it on multiple RPi's; 8 RPi's running in above example
-import socket
-import time
-from picamera2 import Picamera2
+
+import cv2
 import imagezmq
 
-sender = imagezmq.ImageSender(connect_to='tcp://172.25.40.37:5555')
+sender = imagezmq.ImageSender(connect_to='tcp://{serv}:5555')
+cap = cv2.VideoCapture(0)
 
-rpi_name = socket.gethostname() # send RPi hostname with each image
-picam = Picamera2()
-picam.start()
-time.sleep(2)  # allow camera sensor to warm up
-while True:  # send images as stream until Ctrl-C
-    image = picam.capture_array()
-    sender.send_image(rpi_name, image)
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        sender.send_image('test', frame)
