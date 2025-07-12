d0df5c4da5f4b3a40de1ddda5059c4402c7abce5
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-07-13 01:09:21 +0900

    研究室単位の処理, json の出力に対応

--- AI/track.py
+++ AI/track.py
@@ -2,38 +2,48 @@
 from ultralytics import YOLO
 import imagezmq
 import datetime
+import json
+import os
 
 model_path = './best_models/0709.pt'
 tracker_yaml = './custom_tracker.yaml'
+disappear_timeout = datetime.timedelta(minutes=1)
+min_duration = datetime.timedelta(minutes=1)
 
 # create hub
 image_hub = imagezmq.ImageHub()
 
-print('Loading model...')
-model = YOLO(model_path)
-print(f'Loaded {model_path}')
+# for each lab
+yolo_models = {}
+tracking_data = {}
 
 ## main step
-tracked_objects = {}
-disappear_timeout = datetime.timedelta(minutes=1)
-#disappear_timeout = datetime.timedelta(seconds=10) ## debug
-
 print('Start Tracking... (C-c to exit)')
 try:
     while True:
         # receive frame
-        rpi_name, frame = image_hub.recv_image()
-        # YOLO
-        results = model.track(source=frame, persist=True, verbose=False, conf=0.5)
-        res = set()
-        if results[0].boxes.id is not None:
-            results[0].save(filename=f"latest.jpg")
-            res = set(results[0].boxes.id.int().cpu().tolist())
-
+        lab_name, frame = image_hub.recv_image()
         current_time = datetime.datetime.now()
-        # update detected objects
-        for obj_id in res:
-            # objects detected for the first time
+
+        # get the YOLO model instance and tracking data for the current lab
+        if lab_name not in yolo_models:
+            print(f"create new instance for lab : {lab_name} ...")
+            yolo_models[lab_name] = YOLO(model_path)
+            print("Done")
+            tracking_data[lab_name] = {}
+        model = yolo_models[lab_name]
+        tracked_objects = tracking_data[lab_name]
+
+        # tracking
+        results = model.track(source=frame, persist=True, verbose=False, conf=0.5)
+
+        detected_ids = set()
+        if results[0].boxes.id is not None:
+            results[0].save(filename=f"{lab_name}_latest.jpg") # debug image
+            detected_ids = set(results[0].boxes.id.int().cpu().tolist())
+
+        # update the last seen time for detected objects
+        for obj_id in detected_ids:
             if obj_id not in tracked_objects:
                 tracked_objects[obj_id] = {
                     'first' : current_time,
@@ -41,21 +51,31 @@ try:
                 }
             else:
                 tracked_objects[obj_id]['last'] = current_time
-        
-        # check objects
-        del_list = []
+
+        # create json log
+        json_log = []
         for obj_id, data in tracked_objects.items():
-            # objects no longer detected
-            if obj_id not in res:
-                # a minute elapsed without detection
-                time_delta = current_time - data['last']
-                if time_delta > disappear_timeout:
-                    # print duration
-                    duration = (data['last'] - data['first']).total_seconds()
-                    print(f"id:{obj_id} -- {duration}s")
-                    del_list.append(obj_id)
-        for obj_id in del_list:
-            del tracked_objects[obj_id]
+            # only add over min_duration
+            if data['last'] - data['first'] > min_duration:
+                end_time = None
+                if current_time - data['last'] > disappear_timeout:
+                    end_time = data['last'].strftime('%Y-%m-%dT%H:%M:%S')
+                entry = {
+                    "id": obj_id,
+                    "start": data['first'].strftime('%Y-%m-%dT%H:%M:%S'),
+                    "end": end_time
+                }
+                json_log.append(entry)
+
+        # write to temporary JSON file
+        output_json = {
+            "lab_name": lab_name,
+            "data": json_log
+        }
+        with open(f"{lab_name}.tmp", 'w') as f:
+            json.dump(output_json, f, indent=4)
+        # rename temporary file
+        os.rename(f"{lab_name}.tmp", f"{lab_name}.json")
 
         image_hub.send_reply(b'OK')
 except KeyboardInterrupt:
