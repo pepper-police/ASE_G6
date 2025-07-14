4c1cca6d8101aca16b69920e74df89eb3ce49832
ten <shimensoka000@gmail.com>
2025-07-14 18:18:00 +0900

    ラグに対応する処理を追記

--- Raspberry_Pi/send_stream.py
+++ Raspberry_Pi/send_stream.py
@@ -4,10 +4,14 @@
 import cv2
 import imagezmq
 
+serv = ''
 sender = imagezmq.ImageSender(connect_to='tcp://{serv}:5555')
 cap = cv2.VideoCapture(0)
 
 while (cap.isOpened()):
+    # 古いフレームを捨てて最新に近いものを取得
+    for _ in range(5):  # バッファにある古いフレームを5枚捨てる
+        cap.grab()
     ret, frame = cap.read()
     if ret == True:
         sender.send_image('test', frame)
