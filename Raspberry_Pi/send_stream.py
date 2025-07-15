c7d4b9af9e3f96456dd507ba4f42bc7702b524b5
Matsumoto <295148f@gs.kochi-tech.ac.jp>
2025-07-15 18:57:56 +0900

    前処理の追加とエラーハンドリング

--- Raspberry_Pi/send_stream.py
+++ Raspberry_Pi/send_stream.py
@@ -1,17 +1,23 @@
-# run this program on each RPi to send a labelled image stream
-# you can run it on multiple RPi's; 8 RPi's running in above example
-
+import time
 import cv2
 import imagezmq
 
-serv = ''
-sender = imagezmq.ImageSender(connect_to='tcp://{serv}:5555')
-cap = cv2.VideoCapture(0)
+serv = 'xxx.xxx.xxx.xxx' # server ip
+lab_name = 'xxxx_lab' 
 
-while (cap.isOpened()):
-    # 古いフレームを捨てて最新に近いものを取得
-    for _ in range(5):  # バッファにある古いフレームを5枚捨てる
-        cap.grab()
-    ret, frame = cap.read()
-    if ret == True:
-        sender.send_image('test', frame)
+sender = imagezmq.ImageSender(connect_to=f'tcp://{serv}:5555')
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+try:
+    while (cap.isOpened()):
+        for _ in range(10): #古いバッファを捨てる
+            cap.grab()
+        ret, frame = cap.read()
+        if ret == True:
+            reply = sender.send_image(lab_name, frame)
+            print(reply.decode())
+            time.sleep(10)
+except KeyboardInterrupt:
+    print('\nKeyboard Interrupt')
